6a18ec2639d4aeecc655e5b2684d26b319f84777
kirstenlucas <kirstenlucas@kirstens-mbp.wifi.uc.edu>
2019-10-01 23:17:29 -0400

    EOD 10/1

--- IS4010Fall2019Assignment05/src/footballFun/FootballFun.java
+++ IS4010Fall2019Assignment05/src/footballFun/FootballFun.java
@@ -21,8 +21,13 @@ package footballFun;
 import java.util.Scanner; // Imports scanner
 
 public class FootballFun {
-
+	
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in); // Creates a scanner to input score
+		System.out.print("Enter football score: "); // Prompts user to enter score
+		double translateScore = input.nextDouble(); // Declares a double to hold the score
+		
 		int correctCount = 0;
 		if (translateScore(2).equals("safety") == true) { correctCount++;}
 		if (translateScore(3).equals("field goal") == true) { correctCount++;}
@@ -33,13 +38,11 @@ public class FootballFun {
 		if (translateScore(1).equals("invalid") == true) { correctCount++;}
 		if (translateScore(10).equals("you must be playing Quidditch ") == true) { correctCount++;}
 		
-		Scanner input = new Scanner(System.in); // Creates a scanner to input score
-			System.out.print("Enter football score: "); // Prompts user to enter score
-			double score = input.nextDouble(); // Declares a double to hold the score
+
 		
 		if (correctCount == 8) {
-			System.out.println("All tests passed");
-		} else {
+			System.out.println("All tests passed");}
+		else {
 			System.out.println("At least one test failed");
 		}
 	}
